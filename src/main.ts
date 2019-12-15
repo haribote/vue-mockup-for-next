cc58dc73eacd5b1a64d21423a8d1ed09ce539e80
KIMURA Tetsuro <haribote.nobody@gmail.com>
2019-12-15 14:24:41 +0900

    chore: use composition-api

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+import VueCompositionApi from '@vue/composition-api'
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
-import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
+
+Vue.use(VueCompositionApi)
 
 new Vue({
   router,
